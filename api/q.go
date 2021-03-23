81e6666b1482cabe07b37a3879909a1cf22fa0c0
hunsh <hunsh.cn@gmail.com>
2021-03-23 23:02:50 +0800

    feat: for vercel

--- api/q.go
+++ api/q.go
@@ -9,23 +9,18 @@ import (
 	"strings"
 )
 
-//go:embed index.html
-var indexHtml string
 var cache map[string]string
 
-func index(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		_, _ = io.WriteString(w, indexHtml)
-		return
-	}
-	r.URL.Path = strings.Replace(r.URL.Path[1:], ":/", "://", 1)
-	if !strings.HasPrefix(r.URL.Path, "https://h5.cyol.com/special/daxuexi/") {
+func q(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("q")
+	url = strings.Replace(url[1:], ":/", "://", 1)
+	if !strings.HasPrefix(url, "https://h5.cyol.com/special/daxuexi/") {
 		w.WriteHeader(http.StatusBadRequest)
 		_, _ = io.WriteString(w, "错误输入")
 		return
 	}
 
-	temp := strings.Split(r.URL.Path, "/")
+	temp := strings.Split(url, "/")
 	temp = temp[:len(temp)-1]
 	path := strings.Join(temp, "/")
 
@@ -33,7 +28,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if v, ok := cache[path]; ok {
 		title = v
 	} else {
-		res, err := http.Get(r.URL.Path)
+		res, err := http.Get(url)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = io.WriteString(w, "服务异常：请求失败")
@@ -59,10 +54,4 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	path += "/images/end.jpg"
 	_, _ = fmt.Fprintf(w, `<html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"><title>%s</title></head><body style="margin:0"><div style="width:100vw;height:100vh;background-image: url(%s);background-size: 100%% 100%%;"></div></body></html>`, title, path)
-}
-
-func main() {
-	cache = make(map[string]string)
-	http.HandleFunc("/", index)
-	_ = http.ListenAndServe(":8090", nil)
 }
