464331dbb3ae2f34e5b41afcfb2488681ac038d3
hunsh <hunsh.cn@gmail.com>
2021-03-24 15:53:23 +0800

    feat: dev server

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	_ "embed"
+	"io"
 	"net/http"
 	"qndxx/api"
 )
 
+//go:embed index.html
+var indexHtml string
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		_, _ = io.WriteString(w, indexHtml)
+	}
+	return
+}
 func main() {
-	http.HandleFunc("/q", handler.Handler)
+	http.HandleFunc("/", handler.Handler)
+	http.HandleFunc("/api/q", handler.Handler)
 	_ = http.ListenAndServe(":8090", nil)
 }
