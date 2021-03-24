3bb3bbe64f4b00a39de6daeaa20bb2e15d2d2e4a
hunsh <hunsh.cn@gmail.com>
2021-03-24 15:53:39 +0800

    feat: add transport to fix vercel network

--- api/q.go
+++ api/q.go
@@ -1,22 +1,27 @@
 package handler
 
 import (
+	"context"
 	_ "embed"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
+	"net"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var cache map[string]string
+var client http.Client
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	url := r.FormValue("q")
 	url = strings.Replace(url, ":/h", "://h", 1)
-	if !strings.HasPrefix(url, "https://h5.cyol.com/special/daxuexi/") {
+	url = strings.Replace(url, "https://", "http://", 1)
+	if !strings.HasPrefix(url, "http://h5.cyol.com/special/daxuexi/") {
 		w.WriteHeader(http.StatusBadRequest)
 		_, _ = io.WriteString(w, "错误输入")
 		return
@@ -31,7 +36,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	if v, ok := cache[path]; ok {
 		title = v
 	} else {
-		res, err := http.Get(url)
+		res, err := client.Get(url)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			_, _ = io.WriteString(w, "服务异常：请求失败 "+err.Error())
@@ -60,5 +65,14 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func init() {
+	dialer := &net.Dialer{
+		Timeout:   6 * time.Second,
+		KeepAlive: 3 * time.Second,
+	}
+	transport := http.Transport{
+		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return dialer.DialContext(ctx, network, "218.1.70.80:80")
+		}}
+	client = http.Client{Transport: &transport}
 	cache = make(map[string]string)
 }
